9e58a0a58f1144e09988558037511faf6802a7fc
Maksym Kantor <max.kantor72@gmail.com>
2022-04-28 22:11:56 +0300

    encode line solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Given a string, return its encoding version.
@@ -10,9 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const combinations = [];
+  let currentChar = str[0];
+  let combination = [];
+  for (let i = 0; i < str.length; i++) {
+    combination.push(currentChar);
+    if (currentChar !== str[i + 1]) {
+      combination.length > 1 ? combinations.push(combination.length + combination[0]) :
+        combinations.push(combination[0]);
+      combination = [];
+      currentChar = str[i + 1];
+    }
+  }
+  return combinations.join('');
 }
 
 module.exports = {
